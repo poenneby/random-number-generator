773df7910adf5837597db4bcab97ca7950506d6d
Peter Onneby <ponneby@xebia.fr>
2018-06-27 09:57:42 +0200

    Factor out duplication

--- generator.sh
+++ generator.sh
@@ -5,5 +5,7 @@ if [ -z "$1" ]; then
   echo "Usage: generator.sh <filename>"
   exit
 fi
-echo $RANDOM > out/$1.out
-echo "Generated ${RANDOM} to out/${1}.out"
+FILE_URI=out/${1}.out
+RANDOM_NUMBER=$RANDOM
+echo $RANDOM_NUMBER > $FILE_URI
+echo "Generated ${RANDOM_NUMBER} to ${FILE_URI}"
